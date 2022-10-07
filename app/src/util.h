e8918a2a499f624f18c6b774c07cd2ef14d300e1
Nick Kraus <nick@nckraus.com>
2022-10-07 10:19:46 -0400

    Add Nanosecond Busy Wait Function

--- app/src/util.h
+++ app/src/util.h
@@ -1,6 +1,8 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
+#include <zephyr.h>
+
 /* tests a condition, and triggers a kernel oops with a log message on failure */
 #define FATAL_CHECK(_cond, _msg)    \
     do {                            \
@@ -9,5 +11,23 @@
             k_oops();               \
         }                           \
     } while (0)
+
+/* busy waits for a set amount of nanoseconds */
+static inline void busy_wait_nanos(uint32_t nanos) {
+    uint32_t start = k_cycle_get_32();
+    uint32_t wait = (uint32_t) (
+        (uint64_t) nanos *
+        (uint64_t) sys_clock_hw_cycles_per_sec() /
+        (uint64_t) NSEC_PER_SEC
+    );
+
+    while (true) {
+        uint32_t current = k_cycle_get_32();
+        /* the subtraction handles uint32 overflow */ 
+        if ((current - start) >= wait) {
+            break;
+        }
+    }
+}
 
 #endif /* __UTIL_H__ */
